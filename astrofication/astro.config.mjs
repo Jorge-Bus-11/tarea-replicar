e7533d5bbb691bcb09129fb7f92c73e4fa34231e
Jorge-Bus-11 <jorgebus1111@gmail.com>
2025-10-17 11:34:57 +0100

    final

--- astrofication/astro.config.mjs
+++ astrofication/astro.config.mjs
@@ -1,17 +1,4 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  prefetch: {
-    defaultStrategy: 'viewport'
-  },
-  image: {
-    responsiveStyles: true,        
-    layout: 'constrained'
-  },
-
-  
+  prefetch: true
 });
